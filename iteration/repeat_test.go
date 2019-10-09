253bc9967507c0b84fe68ae50d5772343076c6a0
Matt Baker <mbaker@shipt.com>
2019-10-09 10:55:52 -0500

    we loopy fam

---
+++ iteration/repeat_test.go
@@ -0,0 +1,12 @@
+package iteration
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 4)
+	expected := "aaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
