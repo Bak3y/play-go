d88dee187162f5f631d88a2b748c6bfd363579d3
Matt Baker <mbaker@shipt.com>
2019-10-09 11:13:38 -0500

    sum stuff

---
+++ arrays/sum.go
@@ -0,0 +1,13 @@
+package main
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	return
+}
---
+++ arrays/sum_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+
+	t.Run("Collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d and want %d given, %v", got, want, numbers)
+		}
+	})
+
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v and want %v", got, want)
+	}
+
+}
